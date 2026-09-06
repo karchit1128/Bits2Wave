88054208502051f248385754190661480833ab62
karchit1128 <karchit1128@users.noreply.github.com>
2026-09-06 23:38:05 +0530

    Update styling of VIEW MISSIONS links to btn btn-primary

--- check_ghosts.py
+++
@@ -1,6 +0,0 @@
-import re
-html = open('problem-statements/index.html', 'r', encoding='utf-8').read()
-p = re.search(r'<div id="pacman".*?</div>', html, re.S)
-g = re.search(r'<div class="ghost".*?</div>', html, re.S)
-print('Pacman:', p.group(0) if p else 'NOT FOUND')
-print('Ghost:', g.group(0) if g else 'NOT FOUND')
